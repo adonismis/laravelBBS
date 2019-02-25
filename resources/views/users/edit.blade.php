b360b074203fe2c932e77406a34b2484760076e7
adonis <adonismis@gmail.com>
2019-02-25 17:35:49 +0800

    上傳頭像

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,8 +14,7 @@
       </div>
 
       <div class="card-body">
-
-        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
           <input type="hidden" name="_method" value="PUT">
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -24,13 +23,25 @@
             <label for="name-field">用戶名</label>
             <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
           </div>
+
           <div class="form-group">
             <label for="email-field">電子郵件</label>
             <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
           </div>
+
           <div class="form-group">
             <label for="introduction-field">個人簡介</label>
             <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+          </div>
+
+          <div class="form-group mb-4">
+            <label for="" class="avatar-label">用戶頭像</label>
+            <input type="file" name="avatar" class="form-control-file">
+
+            @if($user->avatar)
+              <br>
+              <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="200" />
+            @endif
           </div>
           <div class="well well-sm">
             <button type="submit" class="btn btn-primary">保存</button>
